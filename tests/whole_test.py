19479145af4b41a55411c20e8f8d78ed3722a390
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-12-11 23:09:26 +0000

    fix docstring

--- tests/whole_test.py
+++ tests/whole_test.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024 librsync_py project. Released under AGPL-3.0
 # license. Refer to the LICENSE file for details or visit:
 # https://www.gnu.org/licenses/agpl-3.0.en.html
-"""Tests of the exception classes."""
+"""Tests for the whole API."""
 
 from librsync_py import RsSignatureMagic, delta, patch, signature
 
