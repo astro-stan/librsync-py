de46237288c7ba295e496eb7849d08c629cee948
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-12-14 22:29:42 +0000

    remove _internals/common.py

--- src/librsync_py/_internals/common.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) 2024 librsync_py project. Released under AGPL-3.0
-# license. Refer to the LICENSE file for details or visit:
-# https://www.gnu.org/licenses/agpl-3.0.en.html
-from __future__ import annotations
