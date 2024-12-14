304ea310af0dbe86a6a86bd0b35dff7906647ab4
Stanislav Dimov <stanislav.vdimov@gmail.com>
2024-12-14 21:12:29 +0000

    make whole api more flexible

--- src/librsync_py/whole.py
+++ src/librsync_py/whole.py
@@ -12,22 +12,23 @@ from .stream import Delta, Patch, Signature
 
 
 def signature(  # noqa: PLR0913
-    basis: bytes,
+    basis: bytes | bytearray | io.RawIOBase | io.BytesIO,
     chunk_size: int = io.DEFAULT_BUFFER_SIZE,
     file_size: int | None = None,
     sig_type: SignatureType = SignatureType.RK_BLAKE2,
     block_length: int = 0,
     hash_length: int = 0,
 ) -> bytes:
-    """Generate a new signature.
+    """Generate a new signature from a file-like or bytes-like object.
 
-    :param basis: The source file-like object
-    :type basis: bytes
+    :param basis: The source file-like or bytes-like object
+    :type basis: Union[bytes, bytearray, io.RawIOBase, io.BytesIO]
     :param chunk_size: The read chunk size in bytes. For data sizes above
     1GB, good values are typically in the range of 1MB-16MB. Experimentation
     and/or profiling may be needed to achieve optimal results
     :type chunk_size: int
-    :param file_size: The size of the file-like object. Set to `None` if unknown.
+    :param file_size: The size of the file-like or bytes-like object. Set to
+    `None` if unknown.
     :type file_size: int
     :param block_length: The signature block length. Larger values make a
     shorter signature but increase the delta file size. Use 0 for recommended.
@@ -36,11 +37,14 @@ def signature(  # noqa: PLR0913
     make signatures shorter but increase the chance for corruption due to
     hash collisions. Use `0` for maximum or `-1` for minimum.
     :type hash_length: int
-    :returns: The generated signature
+    :returns: The generated signature object
     :rtype: bytes
     """
+    if not isinstance(basis, (io.RawIOBase, io.BytesIO)):
+        basis = io.BytesIO(basis)
+
     return Signature(
-        raw=io.BytesIO(basis),
+        raw=basis,
         buffer_size=chunk_size,
         file_size=file_size,
         signature_type=sig_type,
@@ -50,26 +54,31 @@ def signature(  # noqa: PLR0913
 
 
 def delta(
-    signature: bytes,
-    basis: bytes,
+    signature: bytes | bytearray | io.RawIOBase | io.BytesIO,
+    basis: bytes | bytearray | io.RawIOBase | io.BytesIO,
     chunk_size: int = io.DEFAULT_BUFFER_SIZE,
 ) -> bytes:
-    """Generate a new delta.
+    """Generate a new delta from a file-like or bytes-like object.
 
-    :param signature: The signature file-like object
-    :type signature: bytes
-    :param basis: The source file-like object
-    :type basis: bytes
+    :param signature: The signature file-like or bytes-like object
+    :type signature: Union[bytes, bytearray, io.RawIOBase, io.BytesIO]
+    :param basis: The source file-like or bytes-like object
+    :type basis: Union[bytes, bytearray, io.RawIOBase, io.BytesIO]
     :param chunk_size: The read chunk size in bytes. For data sizes above
     1GB, good values are typically in the range of 1MB-16MB. Experimentation
     and/or profiling may be needed to achieve optimal results
     :type chunk_size: int
-    :returns: The generated delta
+    :returns: The generated delta object
     :rtype: bytes
     """
+    if not isinstance(signature, (io.RawIOBase, io.BytesIO)):
+        signature = io.BytesIO(signature)
+    if not isinstance(basis, (io.RawIOBase, io.BytesIO)):
+        basis = io.BytesIO(basis)
+
     d = Delta(
-        sig_raw=io.BytesIO(signature),
-        basis_raw=io.BytesIO(basis),
+        sig_raw=signature,
+        basis_raw=basis,
         buffer_size=chunk_size,
     )
     d.load_signature()
@@ -77,25 +86,30 @@ def delta(
 
 
 def patch(
-    basis: bytes,
-    delta: bytes,
+    basis: bytes | bytearray | io.RawIOBase | io.BytesIO,
+    delta: bytes | bytearray | io.RawIOBase | io.BytesIO,
     chunk_size: int = io.DEFAULT_BUFFER_SIZE,
 ) -> bytes:
-    """Patch a file-like object.
+    """Patch a file-like or bytes-like object.
 
-    :param basis: The source file-like object
-    :type basis: bytes
-    :param delta: The delta file-like object
-    :type delta: bytes
+    :param basis: The source file-like or bytes-like object
+    :type basis: Union[bytes, bytearray, io.RawIOBase, io.BytesIO]
+    :param delta: The delta file-like or bytes-like object
+    :type delta: Union[bytes, bytearray, io.RawIOBase, io.BytesIO]
     :param chunk_size: The read chunk size in bytes. For data sizes above
     1GB, good values are typically in the range of 1MB-16MB. Experimentation
     and/or profiling may be needed to achieve optimal results
     :type chunk_size: int
-    :returns: The patched file-like object
+    :returns: The patched object
     :rtype: bytes
     """
+    if not isinstance(basis, (io.RawIOBase, io.BytesIO)):
+        basis = io.BytesIO(basis)
+    if not isinstance(delta, (io.RawIOBase, io.BytesIO)):
+        delta = io.BytesIO(delta)
+
     return Patch(
-        basis_raw=io.BytesIO(basis),
-        delta_raw=io.BytesIO(delta),
+        basis_raw=basis,
+        delta_raw=delta,
         buffer_size=chunk_size,
     ).read()
--- tests/whole_test.py
+++ tests/whole_test.py
@@ -3,6 +3,8 @@
 # https://www.gnu.org/licenses/agpl-3.0.en.html
 """Tests for the whole API."""
 
+import io
+
 from librsync_py import SignatureType, delta, patch, signature
 
 
@@ -17,6 +19,11 @@ def test_signature() -> None:
 
     assert_result(signature(data), SignatureType.RK_BLAKE2)
     assert_result(signature(data, sig_type=SignatureType.MD4), SignatureType.MD4)
+    assert_result(signature(io.BytesIO(data)), SignatureType.RK_BLAKE2)
+    assert_result(
+        signature(io.BytesIO(data), sig_type=SignatureType.MD4),
+        SignatureType.MD4,
+    )
 
 
 def test_delta() -> None:
@@ -24,9 +31,14 @@ def test_delta() -> None:
     data = b"123" * 100
     old_data = b"432" * 123
 
-    result = delta(signature(old_data), data)
-    assert isinstance(result, bytes)
-    assert len(result) > 0
+    def assert_result(result: bytes) -> None:
+        assert isinstance(result, bytes)
+        assert len(result) > 0
+
+    assert_result(delta(signature(old_data), data))
+    assert_result(
+        delta(io.BytesIO(signature(io.BytesIO(old_data))), io.BytesIO(data))
+    )
 
 
 def test_patch() -> None:
@@ -34,7 +46,10 @@ def test_patch() -> None:
     data = b"123" * 100
     old_data = b"432" * 123
 
-    result = patch(old_data, delta(signature(old_data), data))
-    assert isinstance(result, bytes)
-    assert len(result) > 0
-    assert result == data
+    def assert_result(result: bytes) -> None:
+        assert isinstance(result, bytes)
+        assert len(result) > 0
+        assert result == data
+
+    assert_result(patch(old_data, delta(signature(old_data), data)))
+    assert_result(patch(io.BytesIO(old_data), delta(io.BytesIO(signature(io.BytesIO(old_data))), io.BytesIO(data))))
