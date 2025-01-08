284e386887a3eb0e6fcfd4f3d8a1cad832277c92
Stanislav Dimov <stanislav.vdimov@gmail.com>
2025-01-08 23:06:28 +0000

    add version str test

---
+++ tests/misc.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2024 librsync_py project. Released under AGPL-3.0
+# license. Refer to the LICENSE file for details or visit:
+# https://www.gnu.org/licenses/agpl-3.0.en.html
+"""Miscellaneous tests."""
+
+from librsync_py import LIBRSYNC_VERSION_STR
+
+
+def test_librsync_version_string() -> None:
+    """Test getting the librsync version string."""
+    assert LIBRSYNC_VERSION_STR.startswith("librsync")
