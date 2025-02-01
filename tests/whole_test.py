14a0d506a67ae9e7f8803d29d3ae1cd01da36223
Stanislav Dimov <stanislav.vdimov@gmail.com>
2025-02-01 18:59:14 +0000

    Update copyright notice in files

--- tests/whole_test.py
+++ tests/whole_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 librsync_py project. Released under AGPL-3.0
+# Copyright (c) 2024-2025 librsync-py project. Released under AGPL-3.0
 # license. Refer to the LICENSE file for details or visit:
 # https://www.gnu.org/licenses/agpl-3.0.en.html
 """Tests for the whole API."""
