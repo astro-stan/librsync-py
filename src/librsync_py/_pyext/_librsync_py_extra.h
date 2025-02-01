b121f195b60a6a1d3d1bf8bfdcdcebe45e726285
astro-stan <36302090+astro-stan@users.noreply.github.com>
2025-02-01 19:16:59 +0000

    Update copyright (#11)
    Update copyright notices in source files for the new year

--- src/librsync_py/_pyext/_librsync_py_extra.h
+++ src/librsync_py/_pyext/_librsync_py_extra.h
@@ -2,7 +2,7 @@
  * @file _librsync_py_extra.h
  * @brief Extra pyext defintions. This file is directly parsed by CFFI
  *
- * @copyright Copyright (c) 2024 librsync_py project. Released under AGPL-3.0
+ * @copyright Copyright (c) 2024-2025 librsync-py project. Released under AGPL-3.0
  * license. Refer to the LICENSE file for details or visit:
  * https://www.gnu.org/licenses/agpl-3.0.en.html
  *
